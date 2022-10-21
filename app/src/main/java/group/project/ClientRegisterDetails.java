b78a1e969dd9a132581feecf98b5065181f7ae2b
SMThayil <62395019+SMThayil@users.noreply.github.com>
2022-10-20 22:38:43 -0400

    Fixed Minor Errors

--- app/src/main/java/group/project/ClientRegisterDetails.java
+++ app/src/main/java/group/project/ClientRegisterDetails.java
@@ -2,20 +2,33 @@ package group.project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatEditText;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.BreakIterator;
+
+import group.project.firebase.FireDatabase;
 
 public class ClientRegisterDetails extends AppCompatActivity {
+    private FireDatabase database;
 
     @Override
     protected void onCreate(@Nullable Bundle bundle) {
         super.onCreate(bundle);
         this.setContentView(R.layout.register_client_details);
 
+        this.database = new FireDatabase(FirebaseFirestore.getInstance());
+
+
         this.findViewById(R.id.signUpAsClientButton).setOnClickListener(view -> {
             this.startActivity(new Intent(this, MainActivity.class));
         });
+
     }
 
 }
